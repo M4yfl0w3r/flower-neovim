a2d1a9f717984b4235fbaaef00a3b574daa40fc2
M4yfl0w3r <mr.mayflower15@gmail.com>
2022-05-22 13:17:06 +0200

    disable statusline in file tree

--- lua/setup/lualine.lua
+++ lua/setup/lualine.lua
@@ -15,7 +15,8 @@ require('lualine').setup
   options =
   {
     section_separators = { left = '', right = '' },
-    component_separators = { left = '|', right = '|' }
+    component_separators = { left = '|', right = '|' },
+    disabled_filetypes = { 'NvimTree' }
   }
 }
 
