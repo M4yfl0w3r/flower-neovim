6b729394b0e4a191176a4bc2f93bd358d2ab1c13
M4yfl0w3r <mr.mayflower15@gmail.com>
2024-01-29 06:17:08 +0100

    * ignore latex build files in nvim-tree

--- lua/setup/tree.lua
+++ lua/setup/tree.lua
@@ -1,9 +1,16 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+    filters = {
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        custom = { '*.aux', '*.fdb_latexmk', '*.fls', '*.log', '*.out' },
+    },
+})
 
 vim.api.nvim_create_autocmd(
-  'BufEnter',
-  {
-    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
-    nested = true,
-  }
+    'BufEnter',
+    {
+        command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+        nested = true,
+    }
 )
