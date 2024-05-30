9e7362cd9c7a4904eb3477c2e0c07e6ee4292c17
M4yfl0w3r <mr.mayflower15@gmail.com>
2024-05-30 15:17:23 +0200

    * change the structure & add nvim tree

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 My Neovim configuration written in Lua.
 
 - [Package manager](https://github.com/folke/lazy.nvim)
+- [File explorer](https://github.com/nvim-tree/nvim-tree.lua)
+- [Dev icons](https://github.com/nvim-tree/nvim-web-devicons)
---
+++ lua/mfl/plugins/nvim-tree.lua
@@ -0,0 +1,23 @@
+return {
+    'nvim-tree/nvim-tree.lua',
+    
+    dependencies = {
+        'nvim-tree/nvim-web-devicons'
+    },
+
+    config = function()
+        require('nvim-tree').setup{
+            filters = {
+                dotfiles = false,
+                git_clean = false,
+                no_buffer = false,
+                custom = { '*.aux', '*.fdb_latexmk', '*.fls', '*.log', '*.out', '*.gz', '*.toc', '*.pdf', '*.git' }
+            }
+        }
+
+        vim.api.nvim_create_autocmd(
+            'BufEnter', { command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif", nested = true }
+        )
+
+    end
+}
